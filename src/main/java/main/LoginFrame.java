41363941ba5771919bd1fe1e89fcfa093d22831a
WaverCaz <124487940+WaverCaz@users.noreply.github.com>
2023-06-06 00:32:29 +0800

    Update LoginFrame.java

--- src/main/java/main/LoginFrame.java
+++ src/main/java/main/LoginFrame.java
@@ -19,6 +19,7 @@ public class LoginFrame {
         passwordTextField = new JTextField(10);
         usernameLabel = new JLabel("用户名");
         passwordLabel = new JLabel("密码");
+        垃圾啊
     }
 
     public void initJFrame() {
